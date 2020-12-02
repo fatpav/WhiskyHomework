fa403f81665a03087c41f970a0503bd046a3422d
David Murphy <davidmurphy@Davids-MacBook-Pro.local>
2020-12-02 16:45:09 +0000

    Working on whiskies by region

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -1,14 +1,13 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
 import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
 import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,13 +17,30 @@ public class WhiskyController {
     @Autowired
     WhiskyRepository whiskyRepository;
 
+    @Autowired
+    DistilleryRepository distilleryRepository;
+
     @GetMapping(value = "/whiskies")
     public ResponseEntity<List<Whisky>> getAllWhiskies(
-            @RequestParam(name = "year", required = false) Integer year){
+            @RequestParam(name = "year", required = false) Integer year, @RequestParam(name = "distillery", required = false) String distillery, @RequestParam(name = "age", required = false)Integer age){
         if(year != null){
             return new ResponseEntity<>(whiskyRepository.findWhiskyByYear(year), HttpStatus.OK);
         }
+        if (distillery != null & age != null) {
+            Distillery foundDistillery = distilleryRepository.findDistilleryByName(distillery);
+            return new ResponseEntity<>(whiskyRepository.findWhiskyByDistilleryAndAge(foundDistillery, age), HttpStatus.OK);
+        }
         return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
     }
+
+
+//    @GetMapping(value = "/whiskies")
+//    public ResponseEntity<List<Whisky>> findWhiskyByDistilleryAndAge(
+//            @RequestParam(name = "distillery", required = false)String distillery, @RequestParam(name = "age", required = false)Integer age) {
+//        if (distillery != null & age != null) {
+//            return new ResponseEntity<>(whiskyRepository.findWhiskyByDistilleryNameAndAge(distillery, age), HttpStatus.OK);
+//        }
+//        return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
+//    }
 
 }
--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
@@ -1,5 +1,6 @@
 package com.codeclan.example.WhiskyTracker.repositories;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
 import com.codeclan.example.WhiskyTracker.models.Whisky;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +9,8 @@ import java.util.List;
 public interface WhiskyRepository extends JpaRepository<Whisky, Long> {
 
     List<Whisky> findWhiskyByYear(int year);
+    List<Whisky> findWhiskyByAge(int age);
+    List<Whisky> findWhiskyByDistilleryAndAge(Distillery distillery, int age);
+    List<Whisky> findByDistilleryRegion( region);
 }
-//hello
+//
